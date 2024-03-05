a2393b29ca77fc5a7c68d4958747a1c3984dad3c
geoffroy de cooman <geoffroy.decooman@gmail.com>
2024-03-05 16:38:07 +0100

    expert profile interm

--- app/controllers/experts_controller.rb
+++ app/controllers/experts_controller.rb
@@ -1,2 +1,22 @@
 class ExpertsController < ApplicationController
+
+  def new
+    @expert = Expert.new
+  end
+
+  def create
+    @expert = Expert.new(expert_params)
+    @expert.user = current_user
+    if @expert.save
+      redirect_to root_path # path needs to be updated
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def expert_params
+    params.require(:expert).permit(:description, :price_per_hour)
+  end
 end
