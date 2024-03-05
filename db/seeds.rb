4e5737ce24764a9ff1c04bd298b89bbe50da5b1e
Nicolas Neve <nnevedm@gmail.com>
2024-03-05 15:43:47 +0100

    creating fields and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning DB ..."
+User.destroy_all
+Expert.destroy_all
+Request.destroy_all
+Offer.destroy_all
+Field.destroy_all
+puts "DB cleaned"
+
+puts "... creating 15 expertises ..."
+
+jobs = ["Home Maintenance", "Gardening and Landscaping", "Cooking and Baking", "DIY Projects and Crafts", "Technology Setup and Support", "Financial Planning", "Health and Fitness", "Art and Design", "Music and Performing Arts", "Language and Writing", "Sewing and Textile Crafts", "Photography and Videography", "Carpentry and Woodworking", "Automotive Maintenance", "Sustainability and Eco-living"]
+
+jobs.each do |job|
+  Field.create(expertise: job)
+end
+
+puts "... creating 6 users ..."
+
+User.create(email: "thomas@yopmail.com", password: "123456")
+User.create(email: "alice@example.com", password: "123456")
+User.create(email: "bob@example.com", password: "123456")
+User.create(email: "carol@example.com", password: "123456")
+User.create(email: "david@example.com", password: "123456")
+User.create(email: "emma@example.com", password: "123456")
