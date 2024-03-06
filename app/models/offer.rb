8425b682deb56d658bae36e7af27527a8a100dd9
Nicolas Neve <nnevedm@gmail.com>
2024-03-06 15:27:57 +0100

    blala

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,6 +1,5 @@
 class Offer < ApplicationRecord
   belongs_to :request
-
   STATUS = ["Pending", "Declined", "Accepted"]
   validates :content, presence: true, length: { minimum: 25 }
   validates :occurs_on, presence: true
