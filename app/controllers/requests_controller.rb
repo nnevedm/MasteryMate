a19646f9f0464dae4278534e3d978193fe69c0b6
Caroline Deuse <carolinedeuse@gmail.com>
2024-03-06 14:37:33 +0100

    cloudinary and active storage set up, tailored some devise stuff as well

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,12 @@
 class RequestsController < ApplicationController
+
+
+
+  private
+
+  def request_params
+    params.require(:request).permit(:title, :description, :estimated_time, :address, photos: [])
+  end
+
+
 end
