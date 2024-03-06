f1da944f8cb1032c20f79138d204c72fbcbfe283
Nicolas Neve <nnevedm@gmail.com>
2024-03-06 14:43:17 +0100

    show page of a request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,6 @@
 class RequestsController < ApplicationController
+  def show
+    @request = Request.find(params[:id])
+    @requests = Request.where(user: current_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :experts, only: %i[index new create]
+  resources :experts, only: %i[index new create] do
+    resources :requests, only: %i[new create]
+  end
+
+  resources :requests, only: :show
 end
