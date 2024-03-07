2f33c9d76357d9a5bf880be463f8659bf8407f32
geoffroy de cooman <geoffroy.decooman@gmail.com>
2024-03-07 20:42:02 +0100

    added contact details

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :address, presence: true
+  validates :phone_number, presence: true
   validates :photo, presence: true
 
 end
