8dc8a301b4533a6f6ceb02192067573d40f4ccc8
geoffroy de cooman <geoffroy.decooman@gmail.com>
2024-03-07 15:17:10 +0100

    form improvements

--- app/models/request.rb
+++ app/models/request.rb
@@ -5,7 +5,7 @@ class Request < ApplicationRecord
   has_many_attached :pictures
 
   STATUS = ["Pending", "Awaiting", "Accepted"]
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 30 }
   validates :description, presence: true, length: { minimum: 25 }
   validates :status, inclusion: { in: STATUS }
 end
