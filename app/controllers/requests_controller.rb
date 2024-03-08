3d908cbf3e39d2562fc3956cbf745ad0bdf6e6e7
Caroline Deuse <carolinedeuse@gmail.com>
2024-03-08 14:44:51 +0100

    routes created

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,11 +1,25 @@
 class RequestsController < ApplicationController
+
+  # this is for "my requests", the index of user's request
   def index
     @requests = Request.where(user: current_user)
   end
 
+  # this is for "my requests", the show page of user's request
   def show
     @request = Request.find(params[:id])
     @requests = Request.where(user: current_user)
+  end
+
+  # this is for "requests received", the index of all requests an expert received
+  def requests_received
+    @requests = Request.where(expert: current_user)
+  end
+
+  # this is for "requests received", the show of each request an expert received
+  def requests_received_show
+    @request = Request.find(params[:id])
+    @requests = Request.where(expert: current_user)
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,20 @@ Rails.application.routes.draw do
 
   resources :experts, only: %i[index new create] do
     resources :expert_fields, only: [:new, :create]
+    # is the above line needed? I thought we deleted it?
     resources :requests, only: %i[new create]
   end
+
+  # this is for "my requests", the index of user's request
   resources :requests, only: %i[index], as: "my_requests"
+
+  # this is for "my requests", the show page of user's request
   resources :requests, only: %i[show]
+
+  # this is for "requests received", the index of all requests an expert received
+  get "requests_received", to: "requests#requests_received"
+
+  # this is for "requests received", the show of each request an expert received
+  get "requests_received/:id", to: "requests#requests_received_show"
+
 end
