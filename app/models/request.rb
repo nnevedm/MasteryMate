2276aeba58972a3a189b591a766bc68d90b92608
geoffroy de cooman <geoffroy.decooman@gmail.com>
2024-03-08 14:04:40 +0100

    added condition to reveal tab contact

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,10 +1,10 @@
 class Request < ApplicationRecord
   belongs_to :user
   belongs_to :expert
-  has_many :offers
+  has_one :offer
   has_many_attached :pictures
 
-  STATUS = ["Pending", "Awaiting", "Accepted"]
+  STATUS = ["Pending", "Offer made", "Offer declined", "Offer accepted", "Canceled", "Rejected"]
   validates :title, presence: true, length: { maximum: 30 }
   validates :description, presence: true, length: { minimum: 25 }
   validates :status, inclusion: { in: STATUS }
