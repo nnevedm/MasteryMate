8814625187d2894a0e324a2943cf6c4a7dfbadbf
Nicolas Neve <nnevedm@gmail.com>
2024-03-13 14:54:05 +0100

    dynamic offer total price

--- app/javascript/controllers/offer_total_price_controller.js
+++ app/javascript/controllers/offer_total_price_controller.js
@@ -2,18 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="offer-total-price"
 export default class extends Controller {
-  static targets = ["numberOfHour", "extraCosts", "reduction", "totalPrice", "price"]
+  static values = { price: Number }
+  static targets = ["numberOfHour", "extraCosts", "reduction", "totalPrice"]
+
   connect() {
-    console.log(this.numberOfHourTarget)
-    console.log(this.extraCostsTarget)
-    console.log(this.reductionTarget)
-    console.log(this.totalPriceTarget)
-    console.log(this.priceTarget)
+    // console.log(this.totalPriceTarget)
+    // console.log(this.priceTarget)
   }
 
   updateFields() {
-    const totalPricePerHour = this.numberOfHourTarget.innerText * this.priceTarget.innerText
+    const totalPricePerHour = this.numberOfHourTarget.value * this.priceValue
 
-    this.totalPriceTarget.innerText = totalPricePerHour + this.extraCostsTarget.innerText - this.reductionTarget.innerText
+    this.totalPriceTarget.innerText = totalPricePerHour + Number(this.extraCostsTarget.value) - Number(this.reductionTarget.value)
   }
 }
